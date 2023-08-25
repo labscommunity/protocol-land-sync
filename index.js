20965fc7d55c6e56d99c557420aceb5676436aa7
7i7o <mparij@gmail.com>
2023-08-25 14:32:10 -0300

    migrate archiver -> jszip

--- index.js
+++ index.js
@@ -1,107 +1,64 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var fs = require("fs");
-var archiver = require("archiver");
-var path = require("path");
-var PATH = '.';
-var FILENAME = 'repo.zip';
+const fs = require('fs');
+const path = require('path');
+const JSZip = require('jszip');
+
+const PATH = '.';
+const FILENAME = 'repo.zip';
+
 function loadIgnoreList() {
-    var gitignorePath = path.join(PATH, '.gitignore');
+    const gitignorePath = path.join(PATH, '.gitignore');
     if (fs.existsSync(gitignorePath)) {
-        var gitignoreContent = fs.readFileSync(gitignorePath, 'utf-8');
+        const gitignoreContent = fs.readFileSync(gitignorePath, 'utf-8');
         return gitignoreContent
             .split('\n')
-            .map(function (line) { return line.trim(); })
-            .filter(function (line) { return line && !line.startsWith('#'); });
+            .map((line) => line.trim())
+            .filter((line) => line && !line.startsWith('#'));
     }
     return [];
 }
-function zipRepo() {
-    return __awaiter(this, void 0, void 0, function () {
-        var ignoreList;
-        return __generator(this, function (_a) {
-            ignoreList = loadIgnoreList();
-            return [2 /*return*/, new Promise(function (resolve, reject) {
-                    var archive = archiver('zip', { zlib: { level: 9 } });
-                    var chunks = [];
-                    archive.on('data', function (chunk) {
-                        chunks.push(chunk);
-                    });
-                    archive.on('end', function () {
-                        var zipBuffer = Buffer.concat(chunks);
-                        console.log('Repository zipped successfully');
-                        resolve(zipBuffer);
-                    });
-                    archive.on('error', function (error) {
-                        console.error('Error zipping repository:', error);
-                        reject(error);
-                    });
-                    // glob has options to ignore files
-                    archive.glob("{.,}**/{.,}*", {
-                        cwd: PATH,
-                        ignore: ignoreList,
-                        skip: ignoreList,
-                    });
-                    archive.finalize();
-                })];
-        });
-    });
-}
-(function () { return __awaiter(void 0, void 0, void 0, function () {
-    var zipBuffer;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0: return [4 /*yield*/, zipRepo().catch(function (error) {
-                    process.exit(1);
-                })];
-            case 1:
-                zipBuffer = _a.sent();
-                fs.writeFile(FILENAME, zipBuffer, function (error) {
-                    if (error) {
-                        console.error('Error writing zip buffer to file:', error);
-                        process.exit(1);
-                    }
-                    else {
-                        console.log('Zip buffer written to repo.zip');
-                    }
-                });
-                return [2 /*return*/];
+
+async function zipRepo() {
+    const ignoreList = loadIgnoreList();
+    const zip = new JSZip();
+
+    const filesToInclude = [];
+    const ignoreSet = new Set(ignoreList);
+
+    const walk = (currentPath) => {
+        const items = fs.readdirSync(currentPath);
+
+        for (const item of items) {
+            const itemPath = path.join(currentPath, item);
+
+            if (ignoreSet.has(item)) {
+                continue;
+            }
+
+            if (fs.statSync(itemPath).isDirectory()) {
+                walk(itemPath);
+            } else {
+                filesToInclude.push(itemPath);
+            }
         }
+    };
+
+    walk(PATH);
+
+    for (const file of filesToInclude) {
+        const content = fs.readFileSync(file);
+        const relativePath = path.relative(PATH, file);
+        zip.file(relativePath, content);
+    }
+
+    const zipBuffer = await zip.generateAsync({ type: 'nodebuffer' });
+    fs.writeFileSync(FILENAME, zipBuffer);
+
+    console.log('Repository zipped successfully');
+}
+
+(async () => {
+    await zipRepo().catch((error) => {
+        console.error('Error zipping repository:', error);
+        process.exit(1);
     });
-}); })();
+})();
