be15f5a9ea8620bf78f79fbacd9a7cf4e051184f
7i7o <mparij@gmail.com>
2023-08-25 14:50:26 -0300

    cleanup to only store .git folder

--- index.js
+++ index.js
@@ -3,46 +3,24 @@ const path = require('path');
 const JSZip = require('jszip');
 
 const PATH = '.';
+const DOTGIT_PATH = '.git';
 const FILENAME = 'repo.zip';
 
-function loadIgnoreList() {
-    const gitignorePath = path.join(PATH, '.gitignore');
-    if (fs.existsSync(gitignorePath)) {
-        const gitignoreContent = fs.readFileSync(gitignorePath, 'utf-8');
-        return gitignoreContent
-            .split('\n')
-            .map((line) => line.trim())
-            .filter((line) => line && !line.startsWith('#'));
-    }
-    return [];
-}
-
 async function zipRepo() {
-    const ignoreList = loadIgnoreList();
     const zip = new JSZip();
-
     const filesToInclude = [];
-    const ignoreSet = new Set(ignoreList);
 
     const walk = (currentPath) => {
         const items = fs.readdirSync(currentPath);
 
         for (const item of items) {
             const itemPath = path.join(currentPath, item);
-
-            if (ignoreSet.has(item)) {
-                continue;
-            }
-
-            if (fs.statSync(itemPath).isDirectory()) {
-                walk(itemPath);
-            } else {
-                filesToInclude.push(itemPath);
-            }
+            if (fs.statSync(itemPath).isDirectory()) walk(itemPath);
+            else filesToInclude.push(itemPath);
         }
     };
 
-    walk(PATH);
+    walk(DOTGIT_PATH);
 
     for (const file of filesToInclude) {
         const content = fs.readFileSync(file);
